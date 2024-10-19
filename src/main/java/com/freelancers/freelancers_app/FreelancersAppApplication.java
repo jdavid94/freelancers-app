d685caf5c2de9b662a6d8a0d5284ec08e646dc89
jdavi <jdavid94@hotmail.com>
2024-10-19 13:18:58 +0200

    Init project

---
+++ src/main/java/com/freelancers/freelancers_app/FreelancersAppApplication.java
@@ -0,0 +1,13 @@
+package com.freelancers.freelancers_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelancers/freelancers_app/FreelancersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancers.freelancers_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
